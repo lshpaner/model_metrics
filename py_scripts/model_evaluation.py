aad705b74e2bff2ec306255a1132b3dad6f81698
lshpaner <lshpaner@ucla.edu>
2025-02-11 11:29:41 -0800

    added confusion_matrix plot

--- py_scripts/model_evaluation.py
+++ py_scripts/model_evaluation.py
@@ -1,6 +1,7 @@
 ## Step 1. Load the requisite libraries
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 import sys
 
@@ -13,7 +14,15 @@ from sklearn.linear_model import LogisticRegression
 from model_tuner import Model, loadObjects
 import model_tuner
 
-from model_metrics import summarize_model_performance, plot_calibration_curve
+from eda_toolkit import ensure_directory
+from model_metrics import (
+    summarize_model_performance,
+    plot_calibration_curve,
+    plot_conf_matrix,
+)
+
+plt.ion()  # enables interactive mode
+plt.rcParams["figure.max_open_warning"] = 50  # or some other threshold
 
 ## Step 2. Append the correct paths
 # Add the parent directory to sys.path to access relevant .py scripts
@@ -25,7 +34,20 @@ sys.path.append(".")
 print(f"Model Metrics version: {model_metrics.__version__}")
 print(f"Model Metrics authors: {model_metrics.__author__} \n")
 
-model_path = "../results/"
+## Define base paths
+## `base_path`` represents the parent directory of your current working directory
+base_path = os.path.join(os.pardir)
+## Go up one level from 'notebooks' to the parent directory, then into the
+## 'results' folder
+
+model_path = os.path.join(os.pardir, "model_files")
+image_path_png = os.path.join(base_path, "images", "png_images")
+image_path_svg = os.path.join(base_path, "images", "svg_images")
+
+# Use the function to ensure the 'data' directory exists
+ensure_directory(model_path)
+ensure_directory(image_path_png)
+ensure_directory(image_path_svg)
 
 
 ## Step 3. Load the model object and test data
@@ -56,7 +78,7 @@ model_summary = summarize_model_performance(
     # custom_threshold=0.7,
 )
 
-print(model_summary)
+print(f"\n{model_summary}")
 
 
 ## Step 6. Plot the calibration curve
@@ -77,5 +99,35 @@ plot_calibration_curve(
     show_brier_score=True,
     # grid=True,
     # gridlines=False,
+    save_plot=True,
+    image_path_png=image_path_png,
+    image_path_svg=image_path_svg,
     linestyle_kwgs={"color": "black"},
 )
+
+## Step 7. Plot the confusion matrix
+
+plot_conf_matrix(
+    pipelines_or_models=pipelines_or_models,
+    X=X_test,
+    y=y_test,
+    model_titles=model_titles,
+    cmap="Blues",
+    text_wrap=40,
+    save_plot=True,
+    image_path_png=image_path_png,
+    image_path_svg=image_path_svg,
+    grid=False,
+    n_cols=3,
+    n_rows=1,
+    figsize=(4, 4),
+    show_colorbar=False,
+    # label_fontsize=14,
+    # tick_fontsize=12,
+    inner_fontsize=10,
+    class_report=True,
+    # custom_threshold=0.5,
+    # labels=False,
+)
+
+input("Press ENTER to quit...")
