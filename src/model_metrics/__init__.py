757e2b63572e978d8be41634a16d3b1ed2dc086c
lshpaner <lshpaner@ucla.edu>
2025-09-17 16:31:40 -0700

    Versioned up to 0.0.4a5

--- src/model_metrics/__init__.py
+++ src/model_metrics/__init__.py
@@ -22,7 +22,7 @@ PyPI: https://pypi.org/project/model-metrics/
 Documentation: https://lshpaner.github.io/model_metrics_docs/
 
 
-Version: 0.0.4a4
+Version: 0.0.4a5
 
 """
 
@@ -30,7 +30,7 @@ Version: 0.0.4a4
 __doc__ = detailed_doc
 
 
-__version__ = "0.0.4a4"
+__version__ = "0.0.4a5"
 __author__ = "Leonid Shpaner"
 __email__ = "lshpaner@ucla.edu"
 
